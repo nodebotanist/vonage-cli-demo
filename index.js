043d0f14241ae04cad60abf372a33be91d6c9bad
Mx. Kas Perch <the@nodebotani.st>
2020-06-30 10:00:00 -0500

    nexmo balance command complete

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
+require('dotenv').config()
+const https = require('https')
 const argv = require('yargs')
   .command({
-    command: 'balance',
+    command: 'nexmo balance',
     aliases: ['bal'],
     desc: 'See your Vonage account balance',
     handler: getBalance
   })
   .command({
     command: 'send [recipient] [message]',
-    desc: 'Send an SMS messgage'
+    desc: 'Send an SMS messgage',
+    handler: sendMessage
   })
   .demandCommand(1, 'Please enter a command to continue')
   .help()
   .argv
 
-console.log(argv)
+function getBalance(argv){
+  https.get(`https://rest.nexmo.com/account/get-balance?api_key=${process.env.VONAGE_API_KEY}&api_secret=${process.env.VONAGE_API_SECRET}`, (res) => {
+    let data = ''
 
-function getBalance(){
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      console.log(`You have €${JSON.parse(data).value} in your Nexmo account.`);
+    })
+  })
+}
+
+function sendMessage(argv){
+
 }
